3250f878413042c540be349dfe4afad2b8ddeaae
Deltron <jamiedbuell@gmail.com>
2014-04-05 22:21:11 -0700

    first change

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Buell::Application.config.secret_token = '07b5af3d791470c650f3521784bad438236df68248d2fc5ab2b23275cfeb7c559d41a126ad9e0f0e4642f92ee00eff810fb875e5107291440fb8328d47b2f0c0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Buell::Application.config.session_store :cookie_store, :key => '_Buell_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Buell::Application.config.session_store :active_record_store
